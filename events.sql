442b6048d8a07e370e4d4ff3bd4cd40a4e9f2a89
Aliaksei Domut <119735616+aliakseidomut@users.noreply.github.com>
2024-05-20 21:50:35 +0300

    Update events.sql

--- events.sql
+++ events.sql
@@ -10,7 +10,7 @@ GO
 USE EventManagement;
 GO
 
--- Создание таблицы узлов "Event" (Событие)
+-- РЎРѕР·РґР°РЅРёРµ С‚Р°Р±Р»РёС†С‹ СѓР·Р»РѕРІ "Event" (РЎРѕР±С‹С‚РёРµ)
 CREATE TABLE Event (
     EventID INT IDENTITY NOT NULL,
     EventName NVARCHAR(100) NOT NULL,
@@ -23,7 +23,7 @@ CREATE TABLE Event (
 ) AS NODE;
 GO
 
--- Создание таблицы узлов "User" (Пользователь)
+-- РЎРѕР·РґР°РЅРёРµ С‚Р°Р±Р»РёС†С‹ СѓР·Р»РѕРІ "User" (РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ)
 CREATE TABLE [User] (
     UserID INT IDENTITY NOT NULL,
     UserName NVARCHAR(100) NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE [User] (
 ) AS NODE;
 GO
 
--- Создание таблицы узлов "City" (Город)
+-- РЎРѕР·РґР°РЅРёРµ С‚Р°Р±Р»РёС†С‹ СѓР·Р»РѕРІ "City" (Р“РѕСЂРѕРґ)
 CREATE TABLE City (
     CityID INT IDENTITY NOT NULL,
     CityName NVARCHAR(100) NOT NULL,
@@ -43,27 +43,22 @@ CREATE TABLE City (
 ) AS NODE;
 GO
 
--- Создание таблицы ребер "Organizes" (Организует)
+-- РЎРѕР·РґР°РЅРёРµ С‚Р°Р±Р»РёС†С‹ СЂРµР±РµСЂ "Organizes" (РћСЂРіР°РЅРёР·СѓРµС‚)
 CREATE TABLE Organizes AS EDGE;
 GO
 ALTER TABLE Organizes ADD CONSTRAINT EC_Organizes CONNECTION ([User] to Event);
 
--- Создание таблицы ребер "Participates" (Принимает участие)
+-- РЎРѕР·РґР°РЅРёРµ С‚Р°Р±Р»РёС†С‹ СЂРµР±РµСЂ "Participates" (РџСЂРёРЅРёРјР°РµС‚ СѓС‡Р°СЃС‚РёРµ)
 CREATE TABLE Participates AS EDGE;
 GO
 ALTER TABLE Participates ADD CONSTRAINT EC_Participates CONNECTION ([User] to Event);
 
--- Создание таблицы ребер "Subscribes" (Подписывается)
-CREATE TABLE Subscribes AS EDGE;
-GO
-ALTER TABLE Subscribes ADD CONSTRAINT EC_Subscribes CONNECTION ([User] to Event);
-
--- Создание таблицы ребер "ResidesIn" (Проживает)
+-- РЎРѕР·РґР°РЅРёРµ С‚Р°Р±Р»РёС†С‹ СЂРµР±РµСЂ "ResidesIn" (РџСЂРѕР¶РёРІР°РµС‚)
 CREATE TABLE ResidesIn AS EDGE;
 GO
 ALTER TABLE ResidesIn ADD CONSTRAINT EC_ResidesIn CONNECTION ([User] to City);
 
--- Заполнение таблицы узлов "Event" (Событие)
+-- Р—Р°РїРѕР»РЅРµРЅРёРµ С‚Р°Р±Р»РёС†С‹ СѓР·Р»РѕРІ "Event" (РЎРѕР±С‹С‚РёРµ)
 INSERT INTO Event (EventName, Description, StartDateTime, EndDateTime, Venue, CityID)
 VALUES
   ('Music Festival 1', 'Annual music festival', '2024-07-15 18:00:00', '2024-07-17 23:59:59', 'Central Park', 1),
@@ -77,7 +72,7 @@ VALUES
   ('Art Exhibition 1', 'Contemporary art exhibition', '2024-07-05 10:00:00', '2024-07-05 18:00:00', 'Art Gallery', 2),
   ('Art Exhibition 2', 'Contemporary art exhibition', '2024-07-05 10:00:00', '2024-07-05 18:00:00', 'Art Gallery', 2);
 
--- Заполнение таблицы узлов "User" (Пользователь)
+-- Р—Р°РїРѕР»РЅРµРЅРёРµ С‚Р°Р±Р»РёС†С‹ СѓР·Р»РѕРІ "User" (РџРѕР»СЊР·РѕРІР°С‚РµР»СЊ)
 INSERT INTO [User] (UserName, Email, Password, RegistrationDate, CityID)
 VALUES
   ('John Smith', 'john@example.com', 'password123', '2024-01-01', 1),
@@ -91,7 +86,7 @@ VALUES
   ('Christopher Martin', 'christopher@example.com', 'passwordpqr', '2024-09-01', 3),
   ('Amanda Thompson', 'amanda@example.com', 'passwordstu', '2024-10-01', 1);
 
--- Заполнение таблицы узлов "City" (Город)
+-- Р—Р°РїРѕР»РЅРµРЅРёРµ С‚Р°Р±Р»РёС†С‹ СѓР·Р»РѕРІ "City" (Р“РѕСЂРѕРґ)
 INSERT INTO City (CityName)
 VALUES
   ('New York'),
@@ -109,23 +104,23 @@ VALUES
 VALUES
     (
         (SELECT $node_id FROM [User] WHERE UserID = 1), -- John Smith
-        (SELECT $node_id FROM Event WHERE EventID = 1) -- Music Festival 1
+        (SELECT $node_id FROM Event WHERE EventID = 2) -- Music Festival 2
     ),
     (
         (SELECT $node_id FROM [User] WHERE UserID = 2), -- Emily Johnson
-        (SELECT $node_id FROM Event WHERE EventID = 3) -- Conference 1
+        (SELECT $node_id FROM Event WHERE EventID = 4) -- Conference 2
     ),
     (
         (SELECT $node_id FROM [User] WHERE UserID = 3), -- Michael Davis
-        (SELECT $node_id FROM Event WHERE EventID = 5) -- Movie Night 1
+        (SELECT $node_id FROM Event WHERE EventID = 6) -- Movie Night 2
     ),
     (
         (SELECT $node_id FROM [User] WHERE UserID = 4), -- Jessica Wilson
-        (SELECT $node_id FROM Event WHERE EventID = 7) -- Sports Event 1
+        (SELECT $node_id FROM Event WHERE EventID = 8) -- Sports Event 2
     ),
     (
         (SELECT $node_id FROM [User] WHERE UserID = 5), -- David Brown
-        (SELECT $node_id FROM Event WHERE EventID = 9) -- Art Exhibition 1
+        (SELECT $node_id FROM Event WHERE EventID = 10) -- Art Exhibition 2
     );
 
 	INSERT INTO Participates ($from_id, $to_id)
@@ -148,29 +143,6 @@ VALUES
     ),
     (
         (SELECT $node_id FROM [User] WHERE UserID = 10), -- Amanda Thompson
-        (SELECT $node_id FROM Event WHERE EventID = 10) -- Art Exhibition 2
-    );
-
-	INSERT INTO Subscribes ($from_id, $to_id)
-VALUES
-    (
-        (SELECT $node_id FROM [User] WHERE UserID = 1), -- John Smith
-        (SELECT $node_id FROM Event WHERE EventID = 2) -- Music Festival 2
-    ),
-    (
-        (SELECT $node_id FROM [User] WHERE UserID = 2), -- Emily Johnson
-        (SELECT $node_id FROM Event WHERE EventID = 4) -- Conference 2
-    ),
-    (
-        (SELECT $node_id FROM [User] WHERE UserID = 3), -- Michael Davis
-        (SELECT $node_id FROM Event WHERE EventID = 6) -- Movie Night 2
-    ),
-    (
-        (SELECT $node_id FROM [User] WHERE UserID = 4), -- Jessica Wilson
-        (SELECT $node_id FROM Event WHERE EventID = 8) -- Sports Event 2
-    ),
-    (
-        (SELECT $node_id FROM [User] WHERE UserID = 5), -- David Brown
         (SELECT $node_id FROM Event WHERE EventID = 10) -- Art Exhibition 2
     );
 
@@ -217,67 +189,63 @@ VALUES
         (SELECT $node_id FROM City WHERE CityID = 1) -- New York
     );
 
---5
+	--5
 
--- Пользователи, организующие событие "Music Festival 1":
-SELECT U.UserName AS [Имя пользователя]
-FROM [User] AS U,
-     Organizes AS O,
-     Event AS E
+SELECT U.UserName AS [UserName]
+FROM [User] AS U, 
+     Organizes AS O, 
+     Event AS E, 
+     City AS C
 WHERE MATCH (U-(O)->E)
-      AND E.EventName = 'Music Festival 1';
+      AND E.CityID = C.CityID
+      AND C.CityName = 'New York';
 
--- События, в которых участвует пользователь "Sarah Taylor":
-SELECT E.EventName AS [Название события]
-FROM [User] AS U,
-     Participates AS P,
+
+SELECT E.EventName AS [EventName]
+FROM [User] AS U, 
+     Participates AS P, 
      Event AS E
 WHERE MATCH (U-(P)->E)
       AND U.UserName = 'Sarah Taylor';
 
--- Пользователи, проживающие в городе "New York":
-SELECT U.UserName AS [Имя пользователя]
-FROM [User] AS U,
-     ResidesIn AS R,
+SELECT U.UserName AS [UserName]
+FROM [User] AS U, 
+     ResidesIn AS R, 
      City AS C
 WHERE MATCH (U-(R)->C)
-      AND C.CityName = 'New York';
+      AND C.CityName = 'Chicago';
 
--- События, на которые подписан пользователь "John Smith":
-SELECT E.EventName AS [Название события]
-FROM [User] AS U,
-     Subscribes AS S,
-     Event AS E
-WHERE MATCH (U-(S)->E)
+SELECT C.CityName AS [CityName]
+FROM [User] AS U, 
+     ResidesIn AS R, 
+     City AS C
+WHERE MATCH (U-(R)->C)
       AND U.UserName = 'John Smith';
 
--- Пользователи, организующие любые события в городе "Los Angeles":
-SELECT U.UserName AS [Имя пользователя]
-FROM [User] AS U,
-     Organizes AS O,
-     Event AS E,
-     City AS C
-WHERE MATCH (U-(O)->E) 
-      AND E.CityID = C.CityID
-      AND C.CityName = 'Los Angeles';
+
+SELECT U.UserName AS [UserName]
+FROM [User] AS U, 
+     Participates AS P, 
+     Event AS E
+WHERE MATCH (U-(P)->E)
+      AND E.EventName = 'Music Festival 2';
 
 --6
 
--- Найти кратчайший путь от пользователя "David Taylor" к любому другому пользователю, который следует от 1 до 5 через подписки
-SELECT U1.UserName AS UserName1,
-	STRING_AGG(U2.UserName, '->') WITHIN GROUP (GRAPH PATH) AS UserPath
+--РљСЂР°С‚С‡Р°Р№С€РёР№ РїСѓС‚СЊ РѕС‚ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ "John Smith" Рє РјРµСЂРѕРїСЂРёСЏС‚РёСЏРј Р»СЋР±РѕР№ РґР»РёРЅС‹:
+SELECT U1.UserName AS UserName,
+STRING_AGG(E.EventName, '->') WITHIN GROUP (GRAPH PATH) AS EventPath
 FROM [User] AS U1,
-	[User] FOR PATH AS U2,
-	Subscribes FOR PATH AS S
-WHERE MATCH(SHORTEST_PATH(U1(-(S)->U2){1,5}))
-	AND U1.UserName = 'David Taylor';
+Event FOR PATH AS E,
+Organizes FOR PATH AS P
+WHERE MATCH(SHORTEST_PATH(U1(-(P)->E)+))
+AND U1.UserName = 'John Smith';
 
-
--- Найти кратчайший путь от пользователя "Michael Davis" к любому другому пользователю, который следует хотя бы один раз через участие в событиях
-SELECT U1.UserName AS UserName1,
-	STRING_AGG(U2.UserName, '->') WITHIN GROUP (GRAPH PATH) AS UserPath
+--РљСЂР°С‚С‡Р°Р№С€РёР№ РїСѓС‚СЊ РѕС‚ РїРѕР»СЊР·РѕРІР°С‚РµР»СЏ "Sarah Taylor" Рє РјРµСЂРѕРїСЂРёСЏС‚РёСЏРј РґР»РёРЅС‹ РѕС‚ 1 РґРѕ 3:
+SELECT U1.UserName AS UserName,
+       STRING_AGG(E.EventName, '->') WITHIN GROUP (GRAPH PATH) AS EventPath
 FROM [User] AS U1,
-	[User] FOR PATH AS U2,
-	Participates FOR PATH AS P
-WHERE MATCH(SHORTEST_PATH(U1(-(P)->U2)+))
-	AND U1.UserName = 'Michael Davis';
+     Event FOR PATH AS E,
+     Participates FOR PATH AS P
+WHERE MATCH(SHORTEST_PATH(U1(-(P)->E){1,3}))
+  AND U1.UserName = 'Sarah Taylor';
